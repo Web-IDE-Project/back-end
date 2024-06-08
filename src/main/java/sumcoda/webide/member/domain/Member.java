bbf0e9f6bbc4a3f0f2f2a5bc4d451ec9e898419d
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-09 00:01:43 +0900

    Feat: Member, MemberWorkspace, Workspace 엔티티 생성자 작성 및 수정
    - Member 엔티티에 빌더 패턴 생성자 추가
    
    - MemberWorkspace 엔티티에 빌더 패턴 생성자 추가
    - 생성자 내부에 assignMember(), assignWorkspace() 메서드를 통해서 MemberWorkspace 생성시 Member, Workspace 엔티티와 양방향 연관관계 설정
    
    - 엔티티에 빌더 패턴 생성자 추가
    - 생성자 내부에 assignChatRoom(), 메서드를 통해서 Workspace 생성시 ChatRoom 엔티티와 양방향 연관관계 설정

--- src/main/java/sumcoda/webide/member/domain/Member.java
+++ src/main/java/sumcoda/webide/member/domain/Member.java
@@ -2,11 +2,13 @@ package sumcoda.webide.member.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sumcoda.webide.member.enumerate.Role;
 import sumcoda.webide.memberworkspace.domain.MemberWorkspace;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -43,13 +45,25 @@ public class Member {
     private String email;
 
 
+    // 사용자의 권한을 나타내기위한 role
+    // ex) ADMIN, USER
     @Column
     @Enumerated(EnumType.STRING)
     private Role role;
 
     // 하나의 유저는 여러가지 컨테이너 생성가능 1:N 양방향
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
-    private List<MemberWorkspace> memberWorkspaces;
+    private List<MemberWorkspace> memberWorkspaces = new ArrayList<>();
+
+    // 빌더 패턴 생성자
+    @Builder
+    public Member(String userId, String password, String nickname, String email, Role role) {
+        this.userId = userId;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+        this.role = role;
+    }
 
     // Member 1 <-> N MemberWorkspace
     // 양방향 연관관계 편의 메서드
