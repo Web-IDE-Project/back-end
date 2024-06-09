e816e6361fec2a58bfe4c20576bd47af5c2ad6f8
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-10 00:29:16 +0900

    Feat: SecurityConfig 작성
    - 회원가입시 사용자의 비밀번호를 암호화 하기 위해 SecurityConfig 내에 BCryptPasswordEncoder를 bean으로 등록
    
    - 사용자의 특정 URI에 대한 요청을 허용하기위해 filterChain() 메서드를 커스텀하고 bean으로 등록
    - 모든 사용자에 대해 /api/** 요청 허용

---
+++ src/main/java/sumcoda/webide/common/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package sumcoda.webide.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        // 새로추가
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
