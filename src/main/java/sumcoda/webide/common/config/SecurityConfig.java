746f374d85f4def91369b32824c22f21591eb7fa
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-11 00:38:59 +0900

    Feat: JWT 로그인을 위한 SecurityConfig 코드 추가
    - 필드 추가 : authenticationConfiguration, jwtUtil, memberRepository, refreshTokenService, customAuthenticationEntryPoint, customAccessDeniedHandler
    - authenticationManager() -> 사용자 인증에 필요한 authenticationManager를 반환
    - 커스텀한 customAuthenticationFilter 클래스를 통해 로그인 요청 URI 매핑
    - 커스텀한 JWTFillter를 CustomAuthenticationFilter 앞에서 동작하도록 등록 및 커스텀한 CustomAuthenticationEntryPoint, CustomAccessDeniedHandler 등록
    - 커스텀한 CustomLogoutFilter를 스프링 시큐리티에서 제공하는 기존의 LogoutFilter 앞에서 동작하도록 등록
    - JWT 로그인 방식을 세션의 무상태성을 유지해야하기 때문에 세션 생성 방식을 SessionCreationPolicy.STATELESS로 구현

--- src/main/java/sumcoda/webide/common/config/SecurityConfig.java
+++ src/main/java/sumcoda/webide/common/config/SecurityConfig.java
@@ -3,17 +3,50 @@ package sumcoda.webide.common.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsUtils;
+import sumcoda.webide.member.auth.RefreshTokenService;
+import sumcoda.webide.member.auth.filter.JWTFilter;
+import sumcoda.webide.member.auth.general.*;
+import sumcoda.webide.member.auth.util.JWTUtil;
+import sumcoda.webide.member.repository.MemberRepository;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    // AuthenticationManager 를 반환받기위해 필요한 AuthenticationConfiguration 인스턴스 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final JWTUtil jwtUtil;
+
+    private final MemberRepository memberRepository;
+
+    private final RefreshTokenService refreshTokenService;
+
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+
+
+
+
+    // AuthenticationManager 를 반환하는 메서드 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -22,6 +55,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshTokenService);
+        customAuthenticationFilter.setFilterProcessesUrl("/api/auth/login");
+
+
         // 새로추가
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -31,7 +68,27 @@ public class SecurityConfig {
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated());
+        http.
+                addFilterBefore(new JWTFilter(jwtUtil, memberRepository), CustomAuthenticationFilter.class);
+
+        http
+                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(config -> config
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler));
+
+        // 스프링 시큐리티에서 기본적으로 등록되어있는 LogoutFilter 앞에 위치시켜 CustomLogoutFilter가 먼저 동작되도록 등록한다.
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenService), LogoutFilter.class);
+
+
+                //세션 설정
+                // jwt를 활용하는 방식은 세션을 STATELESS 방식을 활용하기 때문에 반드시 해당 코드를 작성
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+
     }
 }
