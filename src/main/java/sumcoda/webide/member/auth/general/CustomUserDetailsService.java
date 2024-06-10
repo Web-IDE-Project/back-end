f701844cbb97c5208ad1b5ca46bc0c06fa90b23f
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-10 23:39:12 +0900

    Feat: CustomUserDetailsService 작성, CookieUtil 클래스 작성
    - 로그인시 사용자의 username을 바탕으로 DB에 해당 username이 존재하는지 확인하고 만약, 존재한다면 사용자의 정보를 CustomUserDetails에 담아 인증 처리를 하기위한 CustomUserDetailsService 작성
    
    - Refresh Token을 쿠키에 저장하고 관리하기위한 CookieUtil 작성

---
+++ src/main/java/sumcoda/webide/member/auth/general/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package sumcoda.webide.member.auth.general;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sumcoda.webide.member.dto.MemberResponseDTO;
+import sumcoda.webide.member.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberResponseDTO member = memberRepository.findOneByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("해당 아이디를 가진 사용자가 존재하지 않습니다. : " + username));
+
+        if (member != null) {
+            return new CustomUserDetails(member);
+        }
+
+        return null;
+    }
+}
