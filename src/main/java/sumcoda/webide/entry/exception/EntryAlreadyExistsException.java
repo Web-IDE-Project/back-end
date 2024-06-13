c2cec8e33861e6c8a6a7db4e99df4b808692bbf0
geunhokinn <gghh1220@gmail.com>
2024-06-13 22:40:47 +0900

    Feat: EntryAlreadyExistsException, EntryCreateException, EntryFoundException, WorkspaceAccessException, WorkspaceFoundException, EntryExceptionHandler 작성
    - 디렉토리 안에 같은 이름의 파일이 존재하면 커스텀한 예외를 던져주기 위한 EntryAlreadyExistsException 작성
    - 엔트리가 파일이 아닌 디렉토리라면 커스텀한 예외를 던져주기 위한EntryCreateException 작성
    - 워크스페이스 안에 엔트리가 존재하지 않다면 커스텀한 예외를 던져주기 위한 EntryFoundException 작성
    - 유저가 워크스페이스에 권한이 존재하지 않다면 커스텀한 예외를 던져주기 위한 WorkspaceAccessException 작성
    - 워크스페이스가 존재하지 않다면 커스텀한 예외를 던져주기 위한 WorkspaceFoundException 작성
    - 전역적으로 예외를 캐치하기 위한 EntryExceptionHandler 작성

---
+++ src/main/java/sumcoda/webide/entry/exception/EntryAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package sumcoda.webide.entry.exception;
+
+public class EntryAlreadyExistsException extends RuntimeException {
+    public EntryAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/sumcoda/webide/entry/exception/EntryCreateException.java
@@ -0,0 +1,7 @@
+package sumcoda.webide.entry.exception;
+
+public class EntryCreateException extends RuntimeException {
+    public EntryCreateException(String message) {
+        super(message);
+    }
+}
