3df0b91a378a7c8098f620f1443d6ae8044c96cf
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-14 02:38:29 +0900

    fix: QueryDSLConfig.java 코드 수정
    - 기존의 필요하지 않은 어노테이션 삭제

--- src/main/java/sumcoda/webide/common/config/QueryDSLConfig.java
+++ src/main/java/sumcoda/webide/common/config/QueryDSLConfig.java
@@ -5,14 +5,12 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
 
 @Configuration
 @RequiredArgsConstructor
 public class QueryDSLConfig {
 
     private final EntityManager entityManager;
-    private final BeanNameUrlHandlerMapping beanNameHandlerMapping;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
