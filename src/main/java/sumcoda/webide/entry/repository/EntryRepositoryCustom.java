b185acd8e598c114d228e6573b42e6e43f9f53b0
geunhokinn <gghh1220@gmail.com>
2024-06-14 22:30:29 +0900

    Feat: EntryRepository 작성 및 EntryRepositoryCustom, EntryRepositoryCustomImpl 코드 제거
    - workspaceId 와 entryId 를 비교하여 해당하는 Entry 엔티티를 조회하는 findByWorkspaceIdAndId() 메서드 추가
    - parentEntry 와 name 을 비교하여 해당하는 Entry 엔티티를 조회하는 findByParentAndName() 메서드 추가
    
    - QueryDSL을 활용하지 않아 EntryRepositoryCustom, EntryRepositoryCustomImpl 코드 제거

--- src/main/java/sumcoda/webide/entry/repository/EntryRepositoryCustom.java
+++ src/main/java/sumcoda/webide/entry/repository/EntryRepositoryCustom.java
@@ -5,6 +5,4 @@ import sumcoda.webide.entry.domain.Entry;
 import java.util.Optional;
 
 public interface EntryRepositoryCustom {
-    Optional<Entry> findByWorkspaceIdAndEntryId(Long workspaceId, Long entryId);
-    Optional<Entry> findByEntryAndName(Entry parentDirectory, String name);
 }
--- src/main/java/sumcoda/webide/entry/repository/EntryRepositoryCustomImpl.java
+++ src/main/java/sumcoda/webide/entry/repository/EntryRepositoryCustomImpl.java
@@ -12,28 +12,4 @@ import static sumcoda.webide.entry.domain.QEntry.entry;
 @Repository
 @RequiredArgsConstructor
 public class EntryRepositoryCustomImpl implements EntryRepositoryCustom {
-
-    private final JPAQueryFactory queryFactory;
-
-    // workspaceId 와 entryId 를 비교하여 해당하는 Entry 엔티티를 조회하는 메서드
-    @Override
-    public Optional<Entry> findByWorkspaceIdAndEntryId(Long workspaceId, Long entryId) {
-        return Optional.ofNullable(queryFactory.selectFrom(entry)
-                .where(
-                        entry.workspace.id.eq(workspaceId),
-                        entry.id.eq(entryId)
-                )
-                .fetchOne());
-    }
-
-    // parentEntry 와 name 을 비교하여 해당하는 Entry 엔티티를 조회하는 메서드
-    @Override
-    public Optional<Entry> findByEntryAndName(Entry parentEntry, String name) {
-        return Optional.ofNullable(queryFactory.selectFrom(entry)
-                .where(
-                        entry.parent.eq(parentEntry),
-                        entry.name.eq(name)
-                )
-                .fetchOne());
-    }
 }
