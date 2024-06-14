816ca4b8d6cb7b6014101e37cc1d424010b0c141
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-14 23:08:51 +0900

    Feat: 유저 로그인 상태 구현
    - AuthResponseDTO.java
    - 필드 : nickname, imageURL 추가
    
    - AuthService.java
    - findOneByUsername() -> 아이디를 바탕으로 멤버 조회
    
    - 유저 로그인 상태 확인을 위한 AuthController.java 작성
    - isAuthenticated() -> 유저 로그인 상태 확인
    
    AuthResponseDTO

---
+++ src/main/java/sumcoda/webide/member/auth/AuthController.java
@@ -0,0 +1,53 @@
+package sumcoda.webide.member.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sumcoda.webide.member.auth.general.AuthResponseDTO;
+import sumcoda.webide.member.auth.social.CustomOAuth2User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping("/api/auth/status")
+    public ResponseEntity<?> isAuthenticated(Authentication authentication) {
+
+        Map<String, Object> responseData = new HashMap<>();
+
+        String username = "";
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            // OAuth2.0 사용자
+            CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
+            username = oauthUser.getUsername();
+
+            // 그외 사용자
+        } else {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername();
+        }
+
+        AuthResponseDTO authResponseDTO = authService.findOneByUsername(username);
+
+        if (authentication.isAuthenticated()) {
+            responseData.put("userInfo", authResponseDTO);
+            responseData.put("message", "세션이 유효합니다.");
+            return ResponseEntity.status(HttpStatus.OK).body(responseData);
+
+        } else {
+            responseData.put("message", "세션이 만료되었습니다.");
+            responseData.put("status", false);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseData);
+        }
+    }
+}
--- src/main/java/sumcoda/webide/member/auth/general/AuthResponseDTO.java
+++ src/main/java/sumcoda/webide/member/auth/general/AuthResponseDTO.java
@@ -6,12 +6,17 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class LoginResponseDTO {
+public class AuthResponseDTO {
 
     private String username;
 
+    private String nickname;
+
+//    private String imageURL;
+
     @Builder
-    public LoginResponseDTO(String username) {
+    public AuthResponseDTO(String username, String nickname) {
         this.username = username;
+        this.nickname = nickname;
     }
 }
