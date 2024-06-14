5cfe3948112423d8144f300b7a0de0646829fcc5
geunhokinn <gghh1220@gmail.com>
2024-06-14 22:30:16 +0900

    Feat: EntryController, EntryService 작성 - 엔트리 생성에 대한 요청을 캐치하기 위한 EntryController 작성 - 엔트리 생성 요청에 대한 정보를 service로 전달하여 엔트리 생성 로직을 처리 - 필드 : EntryService -> 엔트리 생성 로직 메서드 호출을 위한 필드
    - controller에서 전달 받은 엔트리 정보를 바탕으로 엔트리 생성 로직을 처리하기 위한 createEntry() 메서드 작성
    - 필드 : EntryRepository -> 워크스페이스 안에 엔트리가 존재하는지 확인하기 위한 필드
    - 필드 : WorkspaceRepository -> 워크스페이스가 존재하는지 확인하기 위한 필드
    - 유저가 워크스페이스에 접근 권한이 존재하는지 확인하기 위한 checkUserAccessToWorkspace() 메서드 추가, 기존 메서드에서 Role 까지 체크하는 로직 추가

--- src/main/java/sumcoda/webide/entry/controller/EntryController.java
+++ src/main/java/sumcoda/webide/entry/controller/EntryController.java
@@ -5,31 +5,46 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import sumcoda.webide.entry.dto.request.EntryCreateRequestDTO;
 import sumcoda.webide.entry.dto.request.EntryRenameRequestDTO;
 import sumcoda.webide.entry.dto.response.EntryCreateResponseDTO;
 import sumcoda.webide.entry.service.EntryService;
+import sumcoda.webide.member.auth.social.CustomOAuth2User;
+import sumcoda.webide.workspace.dto.response.WorkspaceEntriesResponseDTO;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/containers")
+@RequestMapping("/api/workspaces")
 @RequiredArgsConstructor
 public class EntryController {
     private final EntryService entryService;
 
-    // 디렉토리 생성
-    @PostMapping("/{containerId}/directories/{directoryId}")
-    public ResponseEntity<EntryCreateResponseDTO> createDirectory(
-            @PathVariable Long containerId,
-            @PathVariable Long directoryId,
-            @RequestBody EntryCreateRequestDTO entryCreateRequestDTO) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String username = userDetails.getUsername();
-        EntryCreateResponseDTO response = entryService.createDirectory(containerId, directoryId, entryCreateRequestDTO, username);
+    // 엔트리 생성
+    @PostMapping("/{workspaceId}/entries/{parentId}")
+    public ResponseEntity<List<WorkspaceEntriesResponseDTO>> createEntry(
+            @PathVariable Long workspaceId,
+            @PathVariable Long parentId,
+            @RequestBody EntryCreateRequestDTO entryCreateRequestDTO,
+            Authentication authentication) {
+
+        String username = "";
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            // OAuth2.0 사용자
+            CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
+            username = oauthUser.getUsername();
+
+            // 그외 사용자
+        } else {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            username = userDetails.getUsername();
+        }
+
+        List<WorkspaceEntriesResponseDTO> response = entryService.createEntry(workspaceId, parentId, entryCreateRequestDTO, username);
 
         return ResponseEntity.ok(response);
     }
--- src/main/java/sumcoda/webide/entry/service/EntryService.java
+++ src/main/java/sumcoda/webide/entry/service/EntryService.java
@@ -9,60 +9,71 @@ import sumcoda.webide.entry.dto.request.EntryRenameRequestDTO;
 import sumcoda.webide.entry.dto.response.EntryCreateResponseDTO;
 import sumcoda.webide.entry.exception.*;
 import sumcoda.webide.entry.repository.EntryRepository;
+import sumcoda.webide.memberworkspace.enumerate.MemberWorkspaceRole;
 import sumcoda.webide.workspace.domain.Workspace;
+import sumcoda.webide.workspace.dto.response.WorkspaceEntriesResponseDTO;
 import sumcoda.webide.workspace.exception.WorkspaceAccessException;
 import sumcoda.webide.workspace.exception.WorkspaceFoundException;
 import sumcoda.webide.workspace.repository.WorkspaceRepository;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class EntryService {
     private final EntryRepository entryRepository;
     private final WorkspaceRepository workspaceRepository;
 
-    // 디렉토리 생성
-    public EntryCreateResponseDTO createDirectory(Long containerId, Long directoryId, EntryCreateRequestDTO entryCreateRequestDTO, String username) {
+    // 엔트리 생성(디렉토리 또는 파일을 생성)
+    @Transactional
+    public List<WorkspaceEntriesResponseDTO> createEntry(Long workspaceId, Long parentId, EntryCreateRequestDTO entryCreateRequestDTO, String username) {
+
+        String entryName = entryCreateRequestDTO.getName();
+        boolean isDirectory = entryCreateRequestDTO.getIsDirectory();
 
         // 워크스페이스가 존재하는지 확인
-        Workspace workspace = findWorkspaceById(containerId);
+        Workspace workspace = workspaceRepository.findById(workspaceId)
+                .orElseThrow(() -> new WorkspaceFoundException("존재하지 않는 워크스페이스 Id 입니다.: " + workspaceId));
 
         // 유저가 워크스페이스에 권한이 존재하는지 확인
         checkUserAccessToWorkspace(workspace, username);
 
         // 워크스페이스 안에 엔트리가 존재하는지 확인
-        Entry directory = entryRepository.findByWorkspaceIdAndEntryId(containerId, directoryId)
-                .orElseThrow(() -> new EntryFoundException("워크스페이스에 존재하지 않는 디렉토리 Id 입니다.: " + directoryId));
+        Entry parentEntry = entryRepository.findByWorkspaceIdAndId(workspaceId, parentId)
+                .orElseThrow(() -> new EntryFoundException("워크스페이스에 존재하지 않는 엔트리 Id 입니다.: " + parentId));
 
         // 엔트리가 디렉토리인지 확인
-        if (!directory.getIsDirectory()) {
+        if (!parentEntry.getIsDirectory()) {
             throw new EntryCreateException("파일에 디렉토리를 생성할 수 없습니다.");
         }
 
-        // 디렉토리 안에 같은 이름의 디렉토리가 존재하는지 확인
-        if (entryRepository.findByEntryAndName(directory, entryCreateRequestDTO.getName()).isPresent()) {
-            throw new EntryAlreadyExistsException("같은 이름의 디렉토리가 이미 존재합니다.: " + entryCreateRequestDTO.getName());
+        // 파일 또는 디렉토리 이름의 형식 검증
+        if (entryName.contains(".") && !isDirectory) {
+            throw new EntryAccessException("디렉토리 이름에는 '.' 문자가 포함될 수 없습니다.");
+        } else if (!entryName.contains(".") && isDirectory) {
+            throw new EntryAccessException("파일 이름에는 '.' 문자가 포함되어야 합니다.");
         }
 
-        // 새 디렉토리 생성
-        Entry newDirectory = Entry.createEntry(
-                entryCreateRequestDTO.getName(),
+        // 부모 디렉토리 안에 같은 이름의 엔트리가 존재하는지 확인
+        if (entryRepository.findByParentAndName(parentEntry, entryName).isPresent()) {
+            throw new EntryAlreadyExistsException("같은 이름의 엔트리가 이미 존재합니다.: " + entryName);
+        }
+
+        // 새 엔트리 생성
+        Entry entry = Entry.createEntry(
+                entryName,
                 null,
-                true,
-                directory,
+                isDirectory,
+                parentEntry,
                 workspace
         );
 
-        // 새 디렉토리를 저장
-        entryRepository.save(newDirectory);
+        // 새 엔트리를 저장
+        entryRepository.save(entry);
 
-        // 응답 DTO 생성 및 반환
-        return EntryCreateResponseDTO.builder()
-                .id(newDirectory.getId())
-                .name(newDirectory.getName())
-                .isDirectory(newDirectory.getIsDirectory())
-                .parentId(newDirectory.getParent().getId())
-                .build();
+        //엔트리를 DTO 로 변환하여 반환
+        return workspaceRepository.findAllEntriesByWorkspaceId(workspaceId);
     }
 
     // 디렉토리 삭제
@@ -90,25 +101,6 @@ public class EntryService {
 
         // 디렉토리 삭제
         entryRepository.delete(directory);
-    }
-
-    // 공통 메서드
-
-    // 워크스페이스가 존재하는지 확인
-    private Workspace findWorkspaceById(Long containerId) {
-        return workspaceRepository.findById(containerId)
-                .orElseThrow(() -> new WorkspaceFoundException("존재하지 않는 워크스페이스 Id 입니다.: " + containerId));
-    }
-
-    // 유저가 워크스페이스에 접근 권한이 존재하는지 확인
-    private void checkUserAccessToWorkspace(Workspace workspace, String username) {
-        boolean hasAccess = workspace.getMemberWorkspaces().stream()
-                .anyMatch(mw -> mw.getMember().getUsername().equals(username));
-
-        // 접근 권한이 없으면 예외 발생
-        if (!hasAccess) {
-            throw new WorkspaceAccessException("유저는 워크스페이스에 접근 권한이 없습니다.: " + username);
-        }
     }
 
     // 디렉토리 이름 수정
@@ -141,5 +133,20 @@ public class EntryService {
 
         // 디렉토리 이름 업데이트
         directory.updateName(entryRenameRequestDTO.getName());
+    }
+
+    // 공통 메서드
+
+    // 유저가 워크스페이스에 접근 권한이 존재하는지 확인
+    private void checkUserAccessToWorkspace(Workspace workspace, String username) {
+
+        boolean hasAccess = workspace.getMemberWorkspaces().stream()
+                .anyMatch(mw -> mw.getMember().getUsername().equals(username) &&
+                        mw.getRole().equals(MemberWorkspaceRole.ADMIN));
+
+        // 접근 권한이 없으면 예외 발생
+        if (!hasAccess) {
+            throw new WorkspaceAccessException("유저는 워크스페이스에 접근 권한이 없습니다.: " + username);
+        }
     }
 }
