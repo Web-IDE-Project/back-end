181620e48281412519dd56d482e6413224b73eb3
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-14 20:41:44 +0900

    Feat: WorkspaceRepositoryCustom.java, WorkspaceRepositoryCustomImpl.java 메서드 추가
    - WorkspaceRepositoryCustom.java
    - findWorkspacesByCategory() 추가
    
    - WorkspaceRepositoryCustomImpl.java
    - QueryDSL을 활용하여 findWorkspacesByCategory() 구현
    
    - WorkspaceResponseDTO.java
    - 카테고리별로 조회하는 워크스페이스 데이터를 응답하기 위한  WorkspaceResponseDTO.java 작성
    
    - WorkspaceResponseDAO.java
    - DB에서 카테고리별 워크스페이스 데이터를 조회하여 매핑하기위한 WorkspaceResponseDAO.java 작성

--- src/main/java/sumcoda/webide/workspace/repository/WorkspaceRepositoryCustom.java
+++ src/main/java/sumcoda/webide/workspace/repository/WorkspaceRepositoryCustom.java
@@ -1,10 +1,14 @@
 package sumcoda.webide.workspace.repository;
 
 import sumcoda.webide.workspace.dto.response.WorkspaceEntriesResponseDTO;
+import sumcoda.webide.workspace.dto.response.WorkspaceResponseDAO;
+import sumcoda.webide.workspace.enumerate.Category;
 
 import java.util.List;
 
 public interface WorkspaceRepositoryCustom {
 
     List<WorkspaceEntriesResponseDTO> findAllEntriesByWorkspaceId(Long workspaceId);
+
+    List<WorkspaceResponseDAO> findWorkspacesByCategory(Category category);
 }
--- src/main/java/sumcoda/webide/workspace/repository/WorkspaceRepositoryCustomImpl.java
+++ src/main/java/sumcoda/webide/workspace/repository/WorkspaceRepositoryCustomImpl.java
@@ -1,9 +1,13 @@
 package sumcoda.webide.workspace.repository;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import sumcoda.webide.entry.domain.Entry;
 import sumcoda.webide.workspace.dto.response.WorkspaceEntriesResponseDTO;
+import sumcoda.webide.workspace.dto.response.WorkspaceResponseDAO;
+import sumcoda.webide.workspace.enumerate.Category;
 
 import java.util.HashSet;
 import java.util.List;
@@ -11,7 +15,10 @@ import java.util.Objects;
 import java.util.Set;
 
 import static sumcoda.webide.entry.domain.QEntry.*;
-
+import static sumcoda.webide.member.domain.QMember.member;
+import static sumcoda.webide.memberworkspace.domain.QMemberWorkspace.memberWorkspace;
+import static sumcoda.webide.workspace.domain.QWorkspace.workspace;
+@Slf4j
 @RequiredArgsConstructor
 public class WorkspaceRepositoryCustomImpl implements WorkspaceRepositoryCustom {
 
@@ -29,5 +36,20 @@ public class WorkspaceRepositoryCustomImpl implements WorkspaceRepositoryCustom 
                 .map(entry -> WorkspaceEntriesResponseDTO.fromEntity(entry, idSet))
                 .filter(Objects::nonNull)
                 .toList();
+    }
+
+    @Override
+    public List<WorkspaceResponseDAO> findWorkspacesByCategory(Category category) {
+        return jpaQueryFactory.select(Projections.fields(WorkspaceResponseDAO.class,
+                        workspace.id,
+                        workspace.title,
+                        workspace.language,
+                        workspace.description,
+                        member.nickname))
+                .from(workspace)
+                .leftJoin(workspace.memberWorkspaces, memberWorkspace)
+                .leftJoin(memberWorkspace.member, member)
+                .where(workspace.categories.contains(category))
+                .fetch();
     }
 }
