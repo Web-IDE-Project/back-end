e34c0485867b3aa35ac5134960d14c5a63330f33
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-14 11:52:10 +0900

    Feat: build.gradle 의존성 추가 및 WebIdeApplication.java 코드 추가
    - build.gradle
    - 도커를 활용하기 위한 의존성 추가
    
    - WebIdeApplication.java
    - 애플리케이션 실행시 쉘 스크립트 파일을 실행시키기위한 코드 추가

--- src/main/java/sumcoda/webide/WebIdeApplication.java
+++ src/main/java/sumcoda/webide/WebIdeApplication.java
@@ -1,13 +1,41 @@
 package sumcoda.webide;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@Slf4j
 @SpringBootApplication
-public class WebIdeApplication {
+public class WebIdeApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(WebIdeApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("./docker-build.sh");
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    log.info(line);
+                }
+            }
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new InterruptedException("Docker images build failed.");
+            }
+        } catch (Exception e) {
+            throw new InterruptedException("Failed to build Docker images.");
+        }
+    }
 }
