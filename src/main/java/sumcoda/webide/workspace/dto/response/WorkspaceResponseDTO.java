c93a5ab7f2c29b1e83653146ae10e16c8dbe3101
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-16 16:43:33 +0900

    Fix: WorkspaceResponseDAO, WorkspaceResponseDTO 수정
    - WorkspaceResponseDAO.java
    - 카테고리별로 다른 형태의 데이터를 반환하기 위한 nickname 필드 수정
    - 프로필 이미지 업로드 기능 추가에 따른 awsS3SavedFileURL 필드 추가 및 생성자 필드 추가
    
    - WorkspaceResponseDTO.java
    - 카테고리별로 다른 형태의 데이터를 반환하기 위한 WorkspaceResponseDTO 클래스 내부에 static class 추가 My 작성

--- src/main/java/sumcoda/webide/workspace/dto/response/WorkspaceResponseDTO.java
+++ src/main/java/sumcoda/webide/workspace/dto/response/WorkspaceResponseDTO.java
@@ -5,11 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import sumcoda.webide.workspace.enumerate.Language;
 
 @Slf4j
-
 @Getter
+@NoArgsConstructor
 public class WorkspaceResponseDTO {
 
     private Long id;
@@ -17,20 +16,47 @@ public class WorkspaceResponseDTO {
     private String language;
     private String description;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL) // children 필드가 null 일 때 JSON 반환하지 않도록하는 어노테이션
+    // nickname 필드가 null 일 때 JSON 반환하지 않도록하는 어노테이션
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String nickname;
 
-//    @JsonInclude(JsonInclude.Include.NON_NULL) // children 필드가 null 일 때 JSON 반환하지 않도록하는 어노테이션
-//    private String imageUrl;
+    private String awsS3SavedFileURL;
 
     @Builder
-    public WorkspaceResponseDTO(Long id, String title, String language, String description, String nickname) {
+    public WorkspaceResponseDTO(Long id, String title, String language, String description, String nickname, String awsS3SavedFileURL) {
         this.id = id;
         this.title = title;
         this.language = language;
         this.description = description;
         this.nickname = nickname;
-        log.info(nickname);
-//        this.imageUrl = imageUrl;
+        this.awsS3SavedFileURL = awsS3SavedFileURL;
     }
+
+    @Getter
+    @NoArgsConstructor
+    public static class My {
+        private Long id;
+        private String title;
+        private String language;
+        private String description;
+
+        // nickname 필드가 null 일 때 JSON 반환하지 않도록하는 어노테이션
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private String nickname;
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        private String awsS3SavedFileURL;
+
+        @Builder
+        public My(Long id, String title, String language, String description, String nickname, String awsS3SavedFileURL) {
+            this.id = id;
+            this.title = title;
+            this.language = language;
+            this.description = description;
+            this.nickname = nickname;
+            this.awsS3SavedFileURL = awsS3SavedFileURL;
+        }
+    }
+
+
 }
