146137439ba2a8945c27b57df696ed9f516ae4eb
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-18 04:28:26 +0900

    Refactor: TerminalService.java, TerminalController.java 작성
    - TerminalService.java
    - 터미널에 입력되는 명령어를 분석하고 로직을 수행하고 결과를 출력하기 위한 TerminalService.java 작성
    - 메서드 작성
    - executeCommand() -> 주어진 명령어를 실행하고 결과를 문자열로 반환
    - ls() -> 현재 디렉토리의 파일 및 디렉토리 목록 반환
    - cd() -> 주어진 경로로 이동
    - cat() -> 파일의 내용을 출력
    - touch() -> 새로운 파일을 생성
    - handleCompileAndRunCommand() -> 컴파일 및 실행 명령어 처리
    - getFileExtension() -> 파일 확장자를 가져옴
    - isValidCompileCommand() -> 주어진 명령어와 파일 확장자가 유효한지 확인
    - getCompileSuccessMessage -> 컴파일 성공 메시지를 반환
    - getEntryByPath() -> 경로를 기준으로 엔트리 조회
    - resolvePath() -> 주어진 경로를 기반으로 절대 경로 생성
    - getCurrentPath() -> 세션에서 현재 경로를 가져옴
    - getWorkspaceRootPath() -> 워크스페이스의 루트 경로를 가져옴
    - setCurrentPath() -> 세션에 현재 경로를 설정
    
    - TerminalController.java 작성
    - 각 워크스페이스의 터미널 명령어 실행 요청을 캐치하기 위한 TerminalController.java 작성
    - executeCommand() -> 클라이언트에게서 주어진 명령어를 실행하고 결과를 반환

---
+++ src/main/java/sumcoda/webide/workspace/terminal/TerminalController.java
@@ -0,0 +1,35 @@
+package sumcoda.webide.workspace.terminal;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class TerminalController {
+
+    private final TerminalService terminalService;
+
+    /**
+     * 주어진 명령어를 실행하고 결과를 반환
+     *
+     * @param workspaceId 워크스페이스 ID
+     * @param commandRequestDTO 실행할 명령어
+     * @param session HTTP 세션
+     * @return 명령어 실행 결과
+     */
+    @PostMapping("/api/{workspaceId}/terminal/execute")
+    public ResponseEntity<?> executeCommand(@PathVariable Long workspaceId, @RequestBody CommandRequestDTO commandRequestDTO, HttpSession session) throws IOException, InterruptedException {
+        Map<String, Object> responseData = new HashMap<>();
+
+        String result = terminalService.executeCommand(workspaceId, commandRequestDTO.getCommand(), session);
+        responseData.put("result", result);
+        
+        return ResponseEntity.ok(responseData);
+    }
+}
