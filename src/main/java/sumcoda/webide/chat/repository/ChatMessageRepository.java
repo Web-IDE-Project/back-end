5b3f4827c22445d0c216b83a20c142145ef605e4
geunhokinn <gghh1220@gmail.com>
2024-06-18 20:09:20 +0900

    Feat: ChatMessageRepository 작성
    - 채팅방의 모든 메시지를 조회하는 findByChatRoom() 메서드 추가

--- src/main/java/sumcoda/webide/chat/repository/ChatMessageRepository.java
+++ src/main/java/sumcoda/webide/chat/repository/ChatMessageRepository.java
@@ -2,6 +2,10 @@ package sumcoda.webide.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sumcoda.webide.chat.domain.ChatMessage;
+import sumcoda.webide.chat.domain.ChatRoom;
+
+import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByChatRoom(ChatRoom chatRoom);
 }
