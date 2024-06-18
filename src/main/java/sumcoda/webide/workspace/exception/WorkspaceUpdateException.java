f80b197ede0730369add15a43c9a25cadb8ff606
geunhokinn <gghh1220@gmail.com>
2024-06-18 18:49:48 +0900

    Feat: MemberFoundException, WorkspaceFoundException, WorkspaceUpdateException, WorkspaceExceptionHandler 작성 - 존재하지 않은 멤버에 대한 요청이라면 커스텀한 예외를 던져주기 위한 MemberFoundException 작성 - 존재하지 않은 워크스페이스에 대한 요청이라면 커스텀한 예외를 던져주기 위한 WorkspaceFoundException 작성 - 잘못된 워크스페이스 업데이트 요청이라면 커스텀한 예외를 던져주기 위한 WorkspaceUpdateException 작성 - 전역적으로 예외를 캐치하기 위한 EntryExceptionHandler 작성

---
+++ src/main/java/sumcoda/webide/workspace/exception/WorkspaceUpdateException.java
@@ -0,0 +1,7 @@
+package sumcoda.webide.workspace.exception;
+
+public class WorkspaceUpdateException extends RuntimeException {
+    public WorkspaceUpdateException (String message) {
+        super(message);
+    }
+}
--- src/main/java/sumcoda/webide/workspace/general/WorkspaceExceptionHandler.java
+++ src/main/java/sumcoda/webide/workspace/general/WorkspaceExceptionHandler.java
@@ -4,14 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sumcoda.webide.member.exception.MemberFoundException;
 import sumcoda.webide.workspace.exception.WorkspaceAccessException;
+import sumcoda.webide.workspace.exception.WorkspaceFoundException;
 import sumcoda.webide.workspace.exception.WorkspaceNotCreateException;
+import sumcoda.webide.workspace.exception.WorkspaceUpdateException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class WorkspaceExceptionHandler {
+
+    @ExceptionHandler(MemberFoundException.class)
+    public ResponseEntity<Map<String, Object>> memberFoundExceptionHandler(WorkspaceAccessException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
 
     @ExceptionHandler(WorkspaceNotCreateException.class)
     public ResponseEntity<Map<String, Object>> workspaceNotCreateExceptionHandler(WorkspaceNotCreateException ex) {
@@ -25,5 +35,19 @@ public class WorkspaceExceptionHandler {
         Map<String, Object> response = new HashMap<>();
         response.put("message", ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(WorkspaceFoundException.class)
+    public ResponseEntity<Map<String, Object>> workspaceFoundExceptionHandler(WorkspaceFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(WorkspaceUpdateException.class)
+    public ResponseEntity<Map<String, Object>> workspaceUpdateExceptionHandler(WorkspaceUpdateException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
