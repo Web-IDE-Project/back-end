fbc9bd89e9978991cc4cd721eb1b88ecfffd74e5
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-23 18:21:09 +0900

    Fix: cors 설정을 위한 코드 수정
    - CorsMVCConfig.java
    - cors 설정을 위한 코드 수정
    - exposedHeaders() 메서드에 Access-Control-Allow-Origin, Access-Control-Allow-Credentials 추가
    
    - SecurityConfig.java
    - cors 설정을 위한 코드 수정
    - setExposedHeaders() 메서드에 Access-Control-Allow-Origin, Access-Control-Allow-Credentials 추가

--- src/main/java/sumcoda/webide/common/config/CorsMVCConfig.java
+++ src/main/java/sumcoda/webide/common/config/CorsMVCConfig.java
@@ -12,7 +12,7 @@ public class CorsMVCConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://3ever.vercel.app")
                 .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
                 .allowedHeaders("Authorization", "Authorization_Refresh", "Refresh-Token", "Cache-Control", "Content-Type", "X-AUTH-TOKEN", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .exposedHeaders("Content-Type", "X-AUTH-TOKEN", "Authorization", "Authorization_Refresh")
+                .exposedHeaders("Content-Type", "X-AUTH-TOKEN", "Authorization", "Authorization_Refresh", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // pre-flight 요청을 캐싱하는 시간
     }
--- src/main/java/sumcoda/webide/common/config/SecurityConfig.java
+++ src/main/java/sumcoda/webide/common/config/SecurityConfig.java
@@ -31,6 +31,7 @@ import sumcoda.webide.member.auth.social.OAuth2AuthenticationFailureHandler;
 import sumcoda.webide.member.auth.social.OAuth2AuthenticationSuccessHandler;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -68,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("https://3ever.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://3ever.vercel.app"));
 //        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
 //        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Authorization_Refresh", "Refresh-Token", "Cache-Control", "Content-Type"));
 //        configuration.addAllowedHeader("Content-Type");
@@ -87,13 +88,13 @@ public class SecurityConfig {
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Authorization_Refresh", "Refresh-Token", "Cache-Control", "Content-Type", "X-AUTH-TOKEN", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 
         // 클라이언트가 접근할 수 있도록 허용할 응답 헤더 설정
-        configuration.setExposedHeaders(Arrays.asList("Content-Type", "X-AUTH-TOKEN", "Authorization", "Authorization_Refresh"));
+        configuration.setExposedHeaders(Arrays.asList("Content-Type", "X-AUTH-TOKEN", "Authorization", "Authorization_Refresh", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
 
         // 자격 증명을 포함한 요청 허용
         configuration.setAllowCredentials(true);
 
         // 특정 도메인에서의 요청 허용
-        configuration.addAllowedOrigin("https://3ever.vercel.app");
+        configuration.setAllowedOrigins(List.of("https://3ever.vercel.app"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
