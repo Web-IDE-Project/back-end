1b8564437e0406db3d05a1c5556d1b7addec82fd
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-25 10:17:05 +0900

    Test: 배포 테스트를 위한 코드 수정
    - 배포 테스트를 위한 코드 수정

--- src/main/java/sumcoda/webide/WebIdeApplication.java
+++ src/main/java/sumcoda/webide/WebIdeApplication.java
@@ -18,40 +18,40 @@ public class WebIdeApplication {
     }
 
 
-    @PostConstruct
-    public void init() {
-        try {
-            // docker-build.sh 파일에 실행 권한 부여
-            ProcessBuilder chmodProcessBuilder = new ProcessBuilder("bash", "-c", "chmod +x ./docker-build.sh");
-            Process chmodProcess = chmodProcessBuilder.start();
-            int chmodExitCode = chmodProcess.waitFor();
-            if (chmodExitCode != 0) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(chmodProcess.getErrorStream()))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        log.error(line);
-                    }
-                }
-                throw new InterruptedException("Failed to set executable permission on docker-build.sh.");
-            }
-
-            // docker-build.sh 스크립트 실행
-            ProcessBuilder processBuilder = new ProcessBuilder("./docker-build.sh");
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    log.info(line);
-                }
-            }
-            int exitCode = process.waitFor();
-            if (exitCode != 0) {
-                log.error("Docker build failed with exit code: " + exitCode);
-                throw new InterruptedException("Docker images build failed.");
-            }
-        } catch (Exception e) {
-            log.error("Exception occurred while building Docker images: ", e);
-        }
-    }
+//    @PostConstruct
+//    public void init() {
+//        try {
+//            // docker-build.sh 파일에 실행 권한 부여
+//            ProcessBuilder chmodProcessBuilder = new ProcessBuilder("bash", "-c", "chmod +x ./docker-build.sh");
+//            Process chmodProcess = chmodProcessBuilder.start();
+//            int chmodExitCode = chmodProcess.waitFor();
+//            if (chmodExitCode != 0) {
+//                try (BufferedReader reader = new BufferedReader(new InputStreamReader(chmodProcess.getErrorStream()))) {
+//                    String line;
+//                    while ((line = reader.readLine()) != null) {
+//                        log.error(line);
+//                    }
+//                }
+//                throw new InterruptedException("Failed to set executable permission on docker-build.sh.");
+//            }
+//
+//            // docker-build.sh 스크립트 실행
+//            ProcessBuilder processBuilder = new ProcessBuilder("./docker-build.sh");
+//            processBuilder.redirectErrorStream(true);
+//            Process process = processBuilder.start();
+//            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+//                String line;
+//                while ((line = reader.readLine()) != null) {
+//                    log.info(line);
+//                }
+//            }
+//            int exitCode = process.waitFor();
+//            if (exitCode != 0) {
+//                log.error("Docker build failed with exit code: " + exitCode);
+//                throw new InterruptedException("Docker images build failed.");
+//            }
+//        } catch (Exception e) {
+//            log.error("Exception occurred while building Docker images: ", e);
+//        }
+//    }
 }
