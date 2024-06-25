9f80ff61f2692e0c1d7220e7a96a0304334e36c7
runtime-zer0 <bigstring.zero@gmail.com>
2024-06-25 13:27:28 +0900

    Test: 배포 테스트를 위한 코드 수정
    - 배포 테스트를 위한 코드 수정

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x ./docker-build.sh
+./docker-build.sh
--- src/main/java/sumcoda/webide/WebIdeApplication.java
+++ src/main/java/sumcoda/webide/WebIdeApplication.java
@@ -7,51 +7,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
 @Slf4j
 @SpringBootApplication
-public class WebIdeApplication {
+public class WebIdeApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(WebIdeApplication.class, args);
     }
 
 
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            // docker-build.sh 파일에 실행 권한 부여
-//            ProcessBuilder chmodProcessBuilder = new ProcessBuilder("bash", "-c", "chmod +x ./docker-build.sh");
-//            Process chmodProcess = chmodProcessBuilder.start();
-//            int chmodExitCode = chmodProcess.waitFor();
-//            if (chmodExitCode != 0) {
-//                try (BufferedReader reader = new BufferedReader(new InputStreamReader(chmodProcess.getErrorStream()))) {
-//                    String line;
-//                    while ((line = reader.readLine()) != null) {
-//                        log.error(line);
-//                    }
-//                }
-//                throw new InterruptedException("Failed to set executable permission on docker-build.sh.");
-//            }
-//
-//            // docker-build.sh 스크립트 실행
-//            ProcessBuilder processBuilder = new ProcessBuilder("./docker-build.sh");
-//            processBuilder.redirectErrorStream(true);
-//            Process process = processBuilder.start();
-//            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-//                String line;
-//                while ((line = reader.readLine()) != null) {
-//                    log.info(line);
-//                }
-//            }
-//            int exitCode = process.waitFor();
-//            if (exitCode != 0) {
-//                log.error("Docker build failed with exit code: " + exitCode);
-//                throw new InterruptedException("Docker images build failed.");
-//            }
-//        } catch (Exception e) {
-//            log.error("Exception occurred while building Docker images: ", e);
-//        }
-//    }
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            // init.sh에 실행 권한 부여
+            File initScript = new File("./init.sh");
+            if (!initScript.setExecutable(true)) {
+                throw new InterruptedException("Failed to set executable permission on init.sh.");
+            }
+
+            // init.sh 스크립트 실행
+            ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "./init.sh");
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    log.info(line);
+                }
+            }
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new InterruptedException("init.sh script execution failed.");
+            }
+        } catch (Exception e) {
+            throw new InterruptedException("Failed to execute init.sh script.");
+        }
+    }
 }
